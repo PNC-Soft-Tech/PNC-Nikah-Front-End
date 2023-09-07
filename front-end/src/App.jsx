eb699af8865d1cc4379c681a20f821f45b77d4a4
SobujDasCseBu <88959298+SobujDasCseBu@users.noreply.github.com>
2023-09-07 16:24:22 +0600

    React vite project setup with tailwindcss

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-500 text-[60px] font-bold align-centet self-center  ' >PNC</h1>
+      </div>
+     </>
+  )
+}
+
+export default App
