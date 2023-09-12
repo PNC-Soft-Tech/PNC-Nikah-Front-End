416ef85fbb88b4418f52ffb01d4e9d6f09073b26
anis009 <anis.cse5.bu@gmail.com>
2023-09-12 15:29:58 +0600

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 
 function App() {
