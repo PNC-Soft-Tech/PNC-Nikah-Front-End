d198a725321d5db8ef06b6ddd1d4cc40839d1ea9
SobujDasCseBu <88959298+SobujDasCseBu@users.noreply.github.com>
2023-09-14 00:35:19 +0600

    Update Cart.jsx

--- src/pages/EditBiodata/Cart.jsx
+++ src/pages/EditBiodata/Cart.jsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Cart = ({data,handleOptionChange, options , title }) => {
   return (
     <div className="w-[100%] mb-[15px] ">
-    <label className="block text-sm font-medium text-gray-700">{title} <span className='text-[#e84848] text-[20px] font-normal  '>*</span></label>
+    <label className="block font-medium align-start text-start text-[18px] text-gray-700">{title} <span className='text-[#e84848] text-[20px] font-normal  '>*</span></label>
     
     {options?(<select
       value={data}
       onChange={handleOptionChange}
-      className="mt-1 block w-[500px] h-[45px] p-2   border border-gray-300 rounded-md shadow-sm  text-[13px]" // Set input width to 300px
+      className="mt-1 block w-[500px] h-[45px] p-2   border border-gray-300 rounded-md shadow-sm  text-[16px]" // Set input width to 300px
       required // Mark the input as required
     >
       <option value="">নির্বাচন করুন </option>
