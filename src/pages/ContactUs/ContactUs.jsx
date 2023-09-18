0b6e9d11560cca5daca01ed07785bcd994b7848e
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-09-18 07:06:44 +0600

    Contact us, FAQ, About Us, Instructions Pages Added

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,5 +1,82 @@
+// import React from 'react';
+import {  Colors } from '../../constants/colors'; 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div className="bg-gray-400 py-16">
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-semibold mb-4 text-center" style={{color:Colors.titleText}}>যোগাযোগ করুন</h1>
+        <div className="text-center">
+           <p>আপনার যে কোন জিজ্ঞাসা, নিম্নোক্ত ফর্মে পূরণ করে আমাদের কাছে পাঠিয়ে দিন। </p>
+            <p>আমরা শীঘ্রই আপনার সাথে যোগাযোগ করবো ইন শা আল্লাহ। </p>
+        </div>
+        <div className="max-w-lg mx-auto shadow px-8 py-8 rounded">
+          <form>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700 font-semibold mb-2">আপনার নাম <span className="text-red-600">*</span></label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+                placeholder="মো ঃ রাহাত"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">আপনার ইমেইল <span className="text-red-600">*</span></label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+                placeholder="rahat.cse5.bu@gmail.com"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="block text-gray-700 font-semibold mb-2">আপনার মোবাইল নাম্বার <span className="text-red-600">*</span></label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                className="w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+                placeholder="01793278360"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="bio" className="block text-gray-700 font-semibold mb-2">আপনার বায়োডাটা নং</label>
+              <input
+                type="text"
+                id="bio"
+                name="bio"
+                className="w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+                placeholder="PNCM-2102"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block text-gray-700 font-semibold mb-2">আপনি যা লিখে চান <span className="text-red-600">*</span></label>
+              <textarea
+                id="message"
+                name="message"
+                rows="4"
+                className="w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+                placeholder=" আমি একাউন্ট ডিলিট করতে পারতেছি না "
+                required
+              />
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-6 py-3 rounded-md font-semibold hover:bg-blue-600 transition duration-300"
+              style={{backgroundColor:Colors.pncPrimaryColor}}>
+               মেসেজ পাঠান
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
--- src/pages/Instructions/Instructions.jsx
+++ src/pages/Instructions/Instructions.jsx
@@ -1,5 +1,61 @@
+
+
+import { useState } from 'react';
+import {  Colors } from '../../constants/colors'; 
 const Instructions = () => {
-  return <div>Instructions</div>;
+  // Initialize state to keep track of the open/closed status of each Instructions item
+  const [instructionItems, setInstructionItems] = useState([
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    { question: 'What is React?', answer: 'React is a JavaScript library for building user interfaces.' },
+    { question: 'How do I install React?', answer: 'You can install React using npm or yarn.' },
+    // Add more Instruction items as needed
+  ]);
+
+  // Function to toggle the open/closed state of an Instructions item
+  const toggleItem = (index) => {
+    const updatedInstructionItems = [...instructionItems];
+    updatedInstructionItems[index].isOpen = !updatedInstructionItems[index].isOpen;
+    setInstructionItems(updatedInstructionItems);
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto mt-8 mb-8">
+      <h1 className="text-3xl font-semibold mb-4" style={{color:Colors.titleText}}>নির্দেশনা সমুহ</h1>
+      <ul>
+        {instructionItems.map((item, index) => (
+          <li key={index} className="border-b py-4 bg-gray-300 px-8 py-4 mb-4 rounded" style={{borderBottomColor:`rgba(${Colors.titleText}, 0.7)`}}  >
+            <div className="flex justify-between items-center">
+              <h2 onClick={() => toggleItem(index)} className="text-lg font-medium">{item.question}</h2>
+              <button
+                onClick={() => toggleItem(index)}
+                className="text-gray-900 hover:text-gray-900 text-xl"
+              >
+                {item.isOpen ? '-' : '+'}
+              </button>
+            </div>
+            {item.isOpen && <p className="mt-2">{item.answer}</p>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Instructions;
