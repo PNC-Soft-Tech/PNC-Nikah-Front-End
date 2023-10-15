732603be52dbb257017f1fa5770f0b617938b3c1
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-10-15 19:45:41 +0600

    some changes on dashboard eye & dlt btn

--- src/pages/MyReports/MyReports.jsx
+++ src/pages/MyReports/MyReports.jsx
@@ -1,4 +1,6 @@
 import "./MyReports.css";
+import { Button } from "@material-tailwind/react";
+import { FaEye, FaTrash } from "react-icons/fa";
 const MyReports = () => {
   return (
     <div className=" py-12 mx-auto">
@@ -41,109 +43,17 @@ const MyReports = () => {
                     শ্যামলা
                   </td>
                   <td className="px-4 py-2 text-center w-1/7 border-l">
-                    <button className="text-blue-500 hover:text-blue-700 mx-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 inline"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 1v2m0 18v2m-9-9h2m18 0h-2m-9-9v2m0 18v-2M4 12h2m16-3.535-1.414-1.414-4.243 4.243 1.414 1.414 4.243-4.243z"
-                        />
-                      </svg>
-                      View
-                    </button>
-                    <button className="text-red-500 hover:text-red-700 mx-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 inline"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                      Delete
-                    </button>
+                  <Button  color="green" size="xs" className="mr-2">
+            <FaEye size={12} />
+          </Button>
+          <Button color="red" size="xs">
+            <FaTrash size={12} />
+          </Button>
+                  
                   </td>
                 </tr>
 
-                <tr className="border-b">
-                  <td className="px-4 py-2 text-center w-1/7 border-l">2</td>
-                  <td className="px-4 py-2 text-center w-1/7 border-l">
-                    শ্যামলা
-                  </td>
-                  <td className="px-4 py-2 text-center w-1/7 border-l">
-                    গাত্রবর্ণ
-                  </td>
-                  <td className="px-4 py-2 text-center w-1/7 border-l">
-                    শ্যামলা
-                  </td>
-                  <td className="px-4 py-2 text-center w-1/7 border-l">
-                    গাত্রবর্ণ
-                  </td>
-                  <td className="px-4 py-2 text-center w-1/7 border-l">
-                    শ্যামলা
-                  </td>
-                  <td className="px-4 py-2 text-center w-1/7 border-l">
-                    <button className="text-blue-500 hover:text-blue-700 mx-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 inline"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 1v2m0 18v2m-9-9h2m18 0h-2m-9-9v2m0 18v-2M4 12h2m16-3.535-1.414-1.414-4.243 4.243 1.414 1.414 4.243-4.243z"
-                        />
-                      </svg>
-                      View
-                    </button>
-                    <button className="text-red-500 hover:text-red-700 mx-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 inline"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                      Delete
-                    </button>
-                  </td>
-                </tr>
+               
               </tbody>
             </table>
             </div>
