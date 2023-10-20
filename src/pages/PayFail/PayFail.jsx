2f3e6255ab4c3eddcbf7142d394ce04afa5e1039
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-10-21 00:09:53 +0600

    test

--- src/pages/PayFail/PayFail.jsx
+++ src/pages/PayFail/PayFail.jsx
@@ -1,15 +1,35 @@
 import { useSearchParams } from "react-router-dom";
+import styled from 'styled-components';
 
+const Container = styled.div`
+  background-color: #fff;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-top: 50px;
+`;
+
+const Title = styled.h1`
+  color: #f44336;
+`;
+
+const Message = styled.p`
+  color: #555;
+  font-size: 18px;
+  margin: 20px 0;
+`;
 const PayFail = () => {
 	const [searchParams] = useSearchParams();
 	console.log(searchParams);
 	const message = searchParams.get("message");
-	return (
-		<div>
-			<h1>Failed !!</h1>
-			{message && <p>{message}</p>}
-		</div>
-	);
+	return(
+		<Container>
+		  <Title>Payment Failed!</Title>
+		  {message && <Message>{message}</Message>}
+		</Container>
+	  );
 };
 
 export default PayFail;
